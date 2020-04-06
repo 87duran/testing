61161c3642705d083fa653a463a5156b591f244e
Christopher Duran <87duran@gmail.com>
2020-04-06 15:04:38 -0600

    Delete test.js

--- test.js
+++
@@ -1,6 +0,0 @@
-/* CUSTOM CODE */
-var $scope = angular.element('.column.small-12.ng-binding.ng-scope').scope();
-$scope.data.displayInterestRate = "3.50";
-$scope.data.interestRate = 3.50;
-$scope.calculateMonthlyRate();
-$scope.$apply();
